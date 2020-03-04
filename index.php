e073db854b6d68ea4d1b16b0fa5e3b13a4182222
HritikRaj036 <61770964+HritikRaj036@users.noreply.github.com>
2020-03-04 10:26:07 +0530

    Login page
    Initial commit with completion of login page

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+    	<center>
+    		<h1>Login Here</h1>
+            <form method="post" action="">
+    	        <input type="text" name="email" placeholder="Enter email"/>
+    	        <br/><input type="password" name="password" placeholder="Enter password"/>
+    	        <br/><input type="submit" name="submit" value="login"/>
+            </form>
+        </center>
+    </body>
+</html>
