961410ea55fccc466a4e92d678a5d73a7d29580e
HritikRaj036 <61770964+HritikRaj036@users.noreply.github.com>
2020-03-05 19:57:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!DOCTYPE html>
 <html>
     <head>
         <title></title>
@@ -13,3 +14,25 @@
         </center>
     </body>
 </html>
+<?php
+
+$con = new mysqli("localhost","root","","php_security");
+
+if($con->connect_error) {
+    echo $con->connect_error;
+}
+
+    if (isset($_POST["submit"])) {
+        $email=$_POST["email"];
+        $password=$_POST["password"];
+
+        $sql= "SELECT * FROM user_info WHERE email='$email' AND password='$password'";
+        $run_query = $con->query($sql);
+
+    if ($run_query) {
+            $row=$run_query->fetch_assoc();
+            $email=$row["email"];
+            header("location:profile.php?email=".$email);
+        }    
+    }
+?>
