4148485c9324f2dfae0f1b371bf281bfa714042f
HritikRaj036 <61770964+HritikRaj036@users.noreply.github.com>
2020-03-07 21:37:36 +0530

    Add files via upload
    some php code altered to make sure the entire database is prevented from SQL injection attack

--- index.php
+++ index.php
@@ -22,17 +22,21 @@ if($con->connect_error) {
     echo $con->connect_error;
 }
 
-    if (isset($_POST["submit"])) {
-        $email=$_POST["email"];
-        $password=$_POST["password"];
+    if (isset($_POST["submit"])) 
+    {
+        $pre_stmt=$con->prepare("SELECT * FROM user_info WHERE email=? AND password=?");
+        $pre_stmt->bind_param("ss",$_POST["email"],$_POST["password"]);
+        $pre_stmt->execute();
+        $result=$pre_stmt->get_result();
 
-        $sql= "SELECT * FROM user_info WHERE email='$email' AND password='$password'";
-        $run_query = $con->query($sql);
-
-    if ($run_query) {
-            $row=$run_query->fetch_assoc();
-            $email=$row["email"];
-            header("location:profile.php?email=".$email);
-        }    
+        if($result->num_rows>0)
+        {
+           $row=$result->fetch_assoc();
+           header ("location:profile.php?email=".$row["email"]);
+        }
+        else
+        {
+            echo "login failed";
+        }
     }
 ?>
