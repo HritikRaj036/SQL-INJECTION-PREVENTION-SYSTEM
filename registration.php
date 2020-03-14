e48a96a3bb57f4a29dccc0a92d94a7ca82f603bd
HritikRaj036 <61770964+HritikRaj036@users.noreply.github.com>
2020-03-14 21:51:26 +0530

    Add files via upload
    Added php file for registration of individual accounts

---
+++ registration.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+	    <title>Registration Page</title>
+	    <style type="text/css">
+	    	body
+	    	{
+	    		background-color: lightblue;
+	    	}
+	    </style>
+    </head>
+    <body>
+	    <center>
+		    <h1>Register Here</h1>
+		    <form method="post" action="">
+		        <input type="text" name="email" placeholder="Enter email"/>
+		        </br><input type="password" name="password" placeholder="Enter password"/>
+		        </br><input type="submit" name="Register" value="submit"/>
+	        </form>
+	    </center>
+    </body>
+</html>
