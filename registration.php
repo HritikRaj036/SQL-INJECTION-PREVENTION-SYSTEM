051e888b054869f295ad0671790343480c4e2483
HritikRaj036 <61770964+HritikRaj036@users.noreply.github.com>
2020-03-20 21:57:56 +0530

    Add files via upload
    Added link to index.php for the existing user

--- registration.php
+++ registration.php
@@ -19,7 +19,9 @@
 		    <form method="post" action="">
 		        <input type="text" name="email" placeholder="Enter email"/>
 		        <br><input type="password" name="password" placeholder="Enter password"/>
-		        <br><input type="submit" name="Register" value="submit"/>
+		        <br><input type="submit" name="Register" value="Register"/>
+		        <br><h3>Already registered?</h3>
+		        <a class="btn btn-primary" href="index.php" role="button">Sign In</a>
 	        </form>
 	    </center>
     </body>
