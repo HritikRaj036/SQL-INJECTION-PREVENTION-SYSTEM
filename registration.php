a36b8cb6b0b049c91498592559c06a549a8c87b9
HritikRaj036 <61770964+HritikRaj036@users.noreply.github.com>
2020-04-12 20:02:25 +0530

    Add files via upload
    inserted "required" keyword within input tags

--- registration.php
+++ registration.php
@@ -18,8 +18,8 @@
 	    <center>
 		    <h1>Register Here</h1>
 		    <form method="post" action="connect.php" onsubmit="return Validation()">
-		        <input type="text" name="email" placeholder="Enter email"/>
-		        <br><input type="password" name="password" placeholder="Enter password"/>
+		        <input type="text" name="email" placeholder="Enter email" required />
+		        <br><input type="password" name="password" placeholder="Enter password" required />
 		        <br><input type="submit" name="Register" value="Register"/>
 		        <br><h3>Already registered?</h3>
 		        <a class="btn btn-primary" href="index.php" role="button">Sign In</a>
