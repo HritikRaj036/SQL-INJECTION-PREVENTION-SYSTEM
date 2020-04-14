83095f62b7fd5c060d530e4829190712a60d908c
HritikRaj036 <61770964+HritikRaj036@users.noreply.github.com>
2020-04-14 20:40:15 +0530

    Add files via upload
    Added trim() function for checking any spaces value entered in password column

--- registration.php
+++ registration.php
@@ -17,10 +17,11 @@
 	    	function Validation()
             {
 	            var result = true;
+	            var pass = document.getElementById("pass");
 	            var e = document.getElementsByName("email")[0].value;
                 var atindex = e.indexOf('@');
                 var dotindex = e.lastIndexOf('.');
-	            if(atindex<1 || dotindex>=e.length-2 || dotindex-atindex<3)
+	            if(pass.value.trim()=='' || atindex<1 || dotindex>=e.length-2 || dotindex-atindex<3)
 	            {
 		             result = false;
 	            }
@@ -34,7 +35,7 @@
 		    <h1>Register Here</h1>
 		    <form method="post" action="connect.php" onsubmit="return Validation()">
 		        <input type="text" name="email" placeholder="Enter email" required />
-		        <br><input type="password" name="password" placeholder="Enter password" required />
+		        <br><input type="password" id="pass" placeholder="Enter password" required />
 		        <br><input type="submit" name="Register" value="Register"/>
 		        <br><h3>Already registered?</h3>
 		        <a class="btn btn-primary" href="index.php" role="button">Sign In</a>
