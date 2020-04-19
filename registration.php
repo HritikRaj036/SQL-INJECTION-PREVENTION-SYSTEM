8825f22613ec0d97d06c19ab12fb2cd152a0b485
HritikRaj036 <61770964+HritikRaj036@users.noreply.github.com>
2020-04-19 13:04:20 +0530

    Add files via upload
    added alert boxes with additional CSS properties so that the invalid input by the users can be  easily identified and makes the interface look more attractive and interactive along with addition of labels

--- registration.php
+++ registration.php
@@ -18,13 +18,31 @@
             {
 	            var result = true;
 	            var pass = document.getElementById("pass");
+	            var em = document.getElementById("email");
 	            var e = document.getElementsByName("email")[0].value;
                 var atindex = e.indexOf('@');
                 var dotindex = e.lastIndexOf('.');
-	            if(pass.value.trim()=='' || atindex<1 || dotindex>=e.length-2 || dotindex-atindex<3)
+	            if(pass.value.trim()=='')
 	            {
-		             result = false;
+	                alert("blank password not allowed");
+                    pass.style.border = "solid 3px red";
+                    document.getElementById("lb2").style.visibility="visible";
+		            result = false;
 	            }
+	            else if (em.value.trim()=='') 
+	            {
+                    alert("blank email not allowed");
+                    em.style.border = "solid 3px red";
+                    document.getElementById("lb1").style.visibility="visible";
+                    result = false;
+	            }
+	            else if( atindex<1 || dotindex>=e.length-2 || dotindex-atindex<3)
+	            {
+	            	alert("Invalid Email format");
+	            	em.style.border = "solid 3px red";
+	            	document.getElementById("lb1").style.visibility="visible";
+	            	result = false;
+	            }	
 	            return result;
             }	
 	
@@ -34,8 +52,10 @@
 	    <center>
 		    <h1>Register Here</h1>
 		    <form method="post" action="connect.php" onsubmit="return Validation()">
-		        <input type="text" name="email" placeholder="Enter email" required />
-		        <br><input type="password" id="pass" placeholder="Enter password" required />
+		        <input type="text"  id="email" name="email" placeholder="Enter email" required />
+		        <label id="lb1" style="color: black; visibility: hidden;">Invalid</label>
+		        <br><input type="password" id="pass" name="password" placeholder="Enter password" required />
+		        <label id="lb2" style="color: black; visibility: hidden;">Invalid</label>
 		        <br><input type="submit" name="Register" value="Register"/>
 		        <br><h3>Already registered?</h3>
 		        <a class="btn btn-primary" href="index.php" role="button">Sign In</a>
